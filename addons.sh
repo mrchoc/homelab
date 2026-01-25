569704475a30d59e5755717c3a07c19f46ca0ee4
Ryan Loh <ryanloh28@yahoo.com.sg>
2026-01-25 22:21:23 +0800

    chore: addons script

--- addons.sh
+++ addons.sh
@@ -1,3 +1,5 @@
+# mostly for noting down
+
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
@@ -9,3 +11,9 @@ helm install ingress-nginx ingress-nginx/ingress-nginx \
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.15.2/config/manifests/metallb-native.yaml
 
 curl https://forum.proxmox.com/threads/tutorial-unprivileged-lxcs-mount-cifs-shares.101795/
+
+helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
+helm install sealed-secrets -n {allocated-namespace} sealed-secrets/sealed-secrets --skip-crds -f config.yaml
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
